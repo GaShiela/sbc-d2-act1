d8bc9ff8afd4bce8dc5895908ed047db0bf804b3
GaShiela <shiela.mae.ga@aclcbutuan.edu.ph>
2024-06-18 21:57:02 -0700

    act1

---
+++ summer.py
@@ -0,0 +1,10 @@
+text = input("Word: ")
+print(text.title())
+print(text.capitalize().replace("p", "P"))
+print(text[-8:-4].replace("b", "L"))
+print(text[-4].replace("p", "E"))
+print(text[12].upper() + text
+      [5].upper())
+print(text.replace(" ", "ss").isalpha())
+      
+
